00f470084be5bd290c881a2c5f80b6f15534a1f4
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-07-21 13:04:29 +0700

    feat: follow, unfollow api

--- routers/user.js
+++ routers/user.js
@@ -33,6 +33,8 @@ router.get("/users", async (req, res) => {
       include: {
         posts: true,
         comments: true,
+        followers: true,
+        following: true,
       },
       orderBy: { id: "desc" },
       take: 20,
@@ -51,6 +53,8 @@ router.get("/users/:id", async (req, res) => {
       include: {
         posts: true,
         comments: true,
+        followers: true,
+        following: true,
       },
     });
     res.json(data);
@@ -72,6 +76,40 @@ router.post("/users", async (req, res) => {
       data: { name, username, password: hash, bio },
     });
     res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.post("/follow/:id", auth, async (req, res) => {
+  const user = res.locals.user;
+  const { id } = req.params;
+
+  try {
+    const data = await prisma.follow.create({
+      data: {
+        followerId: Number(user.id),
+        followingId: Number(id),
+      },
+    });
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.delete("/unfollow/:id", auth, async (req, res) => {
+  const user = res.locals.user;
+  const { id } = req.params;
+
+  try {
+    await prisma.follow.deleteMany({
+      where: {
+        followerId: Number(user.id),
+        followingId: Number(id),
+      },
+    });
+    res.json({ msg: `Unfollow user ${id}` });
   } catch (e) {
     res.status(500).json({ error: e });
   }
